cc3f3f5d6f4d6da5c3af22f1c2c61a7da3d3e9c7
Nisha Saini <nisha130599@gmail.com>
2024-08-28 20:57:23 +0530

    - find pivot

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import binarySearch.BinarySearch;
-
 public class Main {
     public static void main(String[] args) {
 
--- src/binarySearch/RotationalListAndPivot.java
+++ src/binarySearch/RotationalListAndPivot.java
@@ -87,4 +87,51 @@ public class RotationalListAndPivot {
 
         return doBinarySearching(arr, target, startIndex, lastIndex);
     }
+
+    /**
+     * handled all the duplicates
+     * */
+    public static int findPivot(int[] arr) {
+        int start = 0, end = arr.length - 1;
+        while(start <= end) {
+            int mid = start+(end-start)/2;
+            if(mid < end && arr[mid] > arr[mid+1]) {
+                return mid;
+            }
+            if(mid > start && arr[mid] < arr[mid-1]) {
+                return mid - 1;
+            }
+
+//check for duplicate elements;
+            if(arr[mid] == arr[start] && arr[end] == arr[mid]){
+                if (start < arr.length-1 && arr[start] > arr[start+1]) {
+                    return start;
+                }
+                start ++;
+                if(end > 0 && arr[end] < arr[end-1]) {
+                    return end - 1;
+                }
+                end --;
+            }
+            if (end < 0) {
+                return -1;
+            }
+            if (start >= arr.length - 1) {
+                return -1;
+            }
+            if(arr[start] < arr[mid] ||
+                    arr[start] == arr[mid] && arr[mid] > arr[end]
+            ) {
+                start = mid +1;
+            }  else if(arr[mid] != arr[end]) {
+                end = mid - 1;
+            } if(arr[end] < arr[mid] ||
+                    arr[end] == arr[mid] && arr[mid] < arr[start]
+            ) {
+                end = mid -1;
+            }
+        }
+
+        return -1;
+    }
 }
